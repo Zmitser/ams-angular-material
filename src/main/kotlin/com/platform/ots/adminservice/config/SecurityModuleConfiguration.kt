a01da4d4764b5c6e3df845d0c5fab31c535b321c
Dmitry Borisovets <gagat@tut.by>
2018-03-19 00:03:13 +0300

    add some security configurations

--- src/main/kotlin/com/platform/ots/adminservice/config/SecurityModuleConfiguration.kt
+++ src/main/kotlin/com/platform/ots/adminservice/config/SecurityModuleConfiguration.kt
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder.FIRST
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.csrf.WebSessionServerCsrfTokenRepository
 
 
 @Configuration
@@ -21,8 +24,49 @@ class SecurityModuleConfiguration(val corsFilter: CorsFilter) {
 
 
     @Bean
-    fun springWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
-        return http.addFilterAt(corsFilter, FIRST).build()
+    fun springWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http
+                .csrf()
+                .csrfTokenRepository(WebSessionServerCsrfTokenRepository())
+
+        http
+                .addFilterAt(corsFilter, FIRST)
+                .exceptionHandling()
+
+        http
+                .httpBasic()
+                .disable()
+
+        http
+                .formLogin()
+                .disable()
+
+        http
+                .logout()
+                .logoutUrl("/api/v1/logout")
+
+        http
+                .headers()
+                .frameOptions()
+                .disable()
+
+        return http
+                .authorizeExchange()
+                .pathMatchers("/api/v1/register").permitAll()
+                .pathMatchers("/api/v1/activate").permitAll()
+                .pathMatchers("/api/v1/authenticate").permitAll()
+                .pathMatchers("/api/v1/account/password-reset/init").permitAll()
+                .pathMatchers("/api/v1/account/password-reset/finish").permitAll()
+                .and()
+                .build()
+
+    }
+
+    @Bean
+    fun userDetailsRepository(): MapReactiveUserDetailsService {
+        val admin = User.withUsername("admin").password("123").roles("ADMIN").build()
+        val ciazhar = User.withUsername("ciazhar").password("123").roles("USER").build()
+        return MapReactiveUserDetailsService(admin, ciazhar)
     }
 
 
